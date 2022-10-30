2d3f709f5111f4b98f2927411384a9a16fd0b87c
Fenil Patel <102594553+fbpatel003@users.noreply.github.com>
2022-10-30 15:35:17 +0530

    Add files via upload

--- src/Components/VideoPlayer.js
+++ src/Components/VideoPlayer.js
@@ -1,11 +1,15 @@
 function VideoPlayer(props) {
+
+  let name = props.videoName;
+  name = name.toUpperCase();
+
   return (
     <>
       <div className="videoPlay">
-        <h1>{props.videoName}</h1>
+        <h1>{name}</h1>
         <iframe
-          width="80%"
-          height="50%"
+          width="853"
+          height="480"
           src={props.videoSrc}
           frameBorder="1"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
